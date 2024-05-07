a0f8b53a8a287084fd6f93aa42eac49bea5c3920
Grinchs <marcisg2345@gmail.com>
2024-05-07 12:17:49 +0300

    Add project files.

---
+++ Iekavetais_rekursija/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iekavetais_rekursija
+{
+    internal class Program
+        {
+        static int Progressija(int n, int diference, int starts)
+            {
+                if (n == 1)
+                {
+                    return starts;
+                }
+                else
+                {
+                    return Progressija(n - 1, diference, starts) + diference;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Cik elementus izvadīt: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.Write("Progresijas solis: ");
+                int diference = int.Parse(Console.ReadLine());
+
+                Console.Write("Progresijas sākums: ");
+                int starts = int.Parse(Console.ReadLine());
+
+                // Izvada aritmētisko progresiju
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write(Progressija(i, diference, starts) + " ");
+                }
+            }
+        }
+    }
